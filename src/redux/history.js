9519cf5cc7c6ca768663facbf4804ecef5516f6b
mocept <dzm1846907072@gmail.com>
2016-08-04 16:30:33 +0800

    ract-router 使用  HashHistory ， 而不是 browserHistory

--- src/redux/history.js
+++ src/redux/history.js
@@ -1,6 +1,7 @@
 import { useRouterHistory } from 'react-router'
-import createBrowserHistory from 'history/lib/createBrowserHistory'
+import createHashHistory from 'history/lib/createHashHistory'
+// browserHistory 另一种路由显示
 
 const historyConfig = { basename: __BASENAME__ }
 
-export default useRouterHistory(createBrowserHistory)(historyConfig)
+export default useRouterHistory(createHashHistory)(historyConfig)
